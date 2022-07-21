78898a09ccb4c36ff95a44941cf0e26cf2f395c7
sonaliamane <97036723+sonaliamane@users.noreply.github.com>
2022-07-21 22:10:48 +0530

    Selenium_java Programs

---
+++ Selenium_java/src/LauchCh.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+
+public class LauchCh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","D:\\Selenium jars\\chromedriver.exe");
+		webDriver driver=new ChromeDriver();
+	}
+
+}
